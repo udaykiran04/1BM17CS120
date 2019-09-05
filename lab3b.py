29a06cb05dacbb0b6950937c43117e6d156cebb9
udaykiran04 <53892004+udaykiran04@users.noreply.github.com>
2019-09-05 12:54:55 +0530

    Update lab3b.py

--- lab3b.py
+++ lab3b.py
@@ -1,13 +1,20 @@
 import random
-l=12
-s=" "
-for i in range(0,12):
-    if(i<=2):
-        s=s+chr(random.randrange(97,123))
-    elif(i>2 and i<5):
-        s=s+chr(random.randrange(65,91))
-    elif(i>5 and i<8):
-        s=s+str(random.randrange(0,10))
-    else:
-        s=s+chr(random.randrange(33,48))
-print(s)
+for j in range(0,4):
+    s=" "
+    s=s+chr(random.randrange(97,123))
+    s=s+chr(random.randrange(65,91))
+    s=s+str(random.randrange(0,10))
+    s=s+chr(random.randrange(33,48))
+    l=random.randrange(10,16)
+    for i in range(4,l):
+        c=random.randrange(1,5)
+        if(c==1):
+            s=s+chr(random.randrange(97,123))
+        elif(c==2):
+            s=s+chr(random.randrange(65,91))
+        elif(c==3):
+            s=s+str(random.randrange(0,10))
+        else:
+            s=s+chr(random.randrange(33,48))
+    print(len(s))
+    print(s)
