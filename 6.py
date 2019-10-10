21e64830e406f0e8ed177c646d0c3613b5a42548
udaykiran04 <53892004+udaykiran04@users.noreply.github.com>
2019-10-10 12:17:46 +0530

    Create 6.py

---
+++ 6.py
@@ -0,0 +1,35 @@
+def check(s):
+    
+    stack=[]
+    stack.append('$')
+    for i in s:
+        if i=='{' or i=='[' or i=='(':
+            stack.append(i);
+
+        elif i=='}':
+            if stack.pop()=='{':
+                pass
+                
+            else:
+                return False    
+
+        elif i==']':
+            if stack.pop()=='[':
+                pass
+            else:
+                return False
+
+        elif i==')':
+            if stack.pop()=='(':
+                pass
+            else:
+                return False    
+            
+    if(stack.pop()=='$'):
+        return True
+    else:
+        return False
+
+
+
+print(check('{}{}[]()'))    
